c589e5f03a71851180061f2ffdddce9dd18b2113
chaitanyab23 <chaitanyabhagat23@gmail.com>
2020-10-06 16:49:49 +0530

    chaitanya

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BioComponent } from './bio/bio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    BioComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
